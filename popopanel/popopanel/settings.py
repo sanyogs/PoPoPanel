a35062723c5cd533564810755ba1a418c4474484
sahil10122002 <151155726+sahil10122002@users.noreply.github.com>
2024-08-06 15:23:19 +0530

    icon images

--- popopanel/popopanel/settings.py
+++ popopanel/popopanel/settings.py
@@ -154,6 +154,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'popo/static'),
+]
+
 STATIC_ROOT = '/var/www/html/popopanel/popopanel/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
